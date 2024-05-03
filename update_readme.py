9e2b72890a319787b5fe673c397b7ae0e81f9cfe
pc5401 <pc5401@naver.com>
2024-05-03 18:43:00 +0900

    testing git action3

--- update_readme.py
+++ update_readme.py
@@ -4,10 +4,14 @@ readme_contents = "# Today I Learned\n\n"
 
 for root, dirs, files in os.walk("TIL"):
     dirs.sort()
+    # "잡동사니" 폴더를 제외하고 싶다면 아래와 같이 처리
+    if "잡동사니" in dirs:
+        dirs.remove("잡동사니")  # 목록에서 "잡동사니" 폴더 제거
     if "README.md" in files:
         path = os.path.relpath(root, "TIL").replace(os.sep, '/')
-        readme_link = os.path.join(path, "README.md").replace(os.sep, '/')
-        readme_contents += f"- [{path}]({readme_link})\n"
+        if "잡동사니" not in path.split('/'):  # 경로에 "잡동사니"가 포함되지 않은 경우에만 추가
+            readme_link = os.path.join(path, "README.md").replace(os.sep, '/')
+            readme_contents += f"- [{path}]({readme_link})\n"
 
 with open("README.md", "w") as f:
     f.write(readme_contents)
