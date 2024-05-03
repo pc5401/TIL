d5ce779d78accbcba49344874e3099086dba86f6
pc5401 <pc5401@naver.com>
2024-05-03 18:24:57 +0900

    test git action2

--- update_readme.py
+++ update_readme.py
@@ -4,11 +4,10 @@ readme_contents = "# Today I Learned\n\n"
 
 for root, dirs, files in os.walk("TIL"):
     dirs.sort()
-    print("Visiting:", root)  # 현재 방문 중인 폴더 출력
     if "README.md" in files:
         path = os.path.relpath(root, "TIL").replace(os.sep, '/')
-        readme_contents += f"- [{path}]({root}/README.md)\n"
-        print("Added to README:", path)  # README에 추가된 항목 출력
+        readme_link = os.path.join(path, "README.md").replace(os.sep, '/')
+        readme_contents += f"- [{path}]({readme_link})\n"
 
 with open("README.md", "w") as f:
     f.write(readme_contents)
