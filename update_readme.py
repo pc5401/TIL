1ec75ed7b3ddb8d4ce258f087f44a9f5088ec697
pc5401 <pc5401@naver.com>
2024-05-03 21:11:06 +0900

    change update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,18 +1,39 @@
 import os
 
-readme_contents = "# Today I Learned\n\n"
 
-for root, dirs, files in os.walk("."):
-    dirs.sort()
-    # "잡동사니" 제외
-    if "잡동사니" in dirs:
-        dirs.remove("잡동사니")
+def get_md_title(file_path: str) -> str:
+    """마크다운 파일에서 첫 줄의 제목을 추출한다."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        first_line = file.readline().strip()
+        if first_line.startswith('#'):
+            return first_line.lstrip('#').strip()  # 제목에서 '#' 기호를 제거하고 반환
+        else:
+            return os.path.basename(file_path)  # 제목이 없는 경우 파일 이름 반환
 
-    if "README.md" in files:
-        path = os.path.relpath(root, ".").replace(os.sep, '/')
-        if "잡동사니" not in path.split('/'):
-            readme_link = os.path.join(path, "README.md").replace(os.sep, '/')
-            readme_contents += f"- [{path}]({readme_link})\n"
 
-with open("README.md", "w") as f:
-    f.write(readme_contents)
+def write_content(path: str, cnt: int):
+    content = ''
+    # 현재 경로의 디렉토리 목록과 파일 목록만 가져오기
+    dirs = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+    for dir in dirs:
+        content += f'{"#" * cnt} [{dir}]({path}/{dir})\n'
+        content += write_content(os.path.join(path, dir), cnt+1)
+
+    for filename in files:
+        content += f'- [{get_md_title(os.path.join(path, filename))}]({path}/{filename})\n'
+
+    return content
+
+def main():
+    readme_contents = "# Today I Learned\n\n\n"
+    root_dir = "./learn"
+    readme_contents += write_content(root_dir, 2)
+    print(readme_contents)
+    with open('README.md', 'w') as f:
+        f.write(readme_contents)
+
+
+if __name__ == "__main__":
+    main()
